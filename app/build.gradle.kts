a9da34e652f08813e0041692a9ed062f634eb411
Saimo via mac <gabrielsaimo68@gmail.com>
2026-09-17 23:38:53 -0300

    Prepara versão 1.6.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         // Alcança os TV Box antigos ainda em uso.
         minSdk = 21
         targetSdk = 35
-        versionCode = 10601
-        versionName = "1.6.1"
+        versionCode = 10602
+        versionName = "1.6.2"
     }
 
     signingConfigs {
